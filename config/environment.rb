168f5cbfa99ff51e3768b008079eb9780a6c3b69
Spencer Lindemuth <lindemut@gmail.com>
2019-06-19 14:51:25 -0700

    Fixed merge conflicts maybe

--- config/environment.rb
+++ config/environment.rb
@@ -15,12 +15,9 @@ require_relative '../lib/models/symbol_validator.rb'
 require_relative '../lib/models/user_interaction.rb'
 require_relative '../lib/models/user.rb'
 require_relative '../lib/models/user_stock_research_menu.rb'
-<<<<<<< HEAD
-=======
 require_relative '../lib/models/login.rb'
 require_relative '../lib/models/user_stock_profile_menu.rb'
 require_relative '../lib/models/stock_full_profile.rb'
->>>>>>> 249160d5e5a90262d9670015ace5b1946c1663cd
 require_relative '../lib/models/user_login.rb'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
