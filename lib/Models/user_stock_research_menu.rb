c117e719ba5e82721d07c3a3412e4fc9e3078be7
Spencer Lindemuth <lindemut@gmail.com>
2019-06-20 09:11:16 -0700

    FIXED EVERYTHING

--- lib/Models/user_stock_research_menu.rb
+++ lib/Models/user_stock_research_menu.rb
@@ -1,5 +1,7 @@
 def user_stock_research_menu(rerun = 0)
-    system('clear')
+    if rerun == 1 || rerun == 0
+        system('clear')
+    end
     if rerun == 2
         puts 'Please enter stock symbol or stock name to lookup or type "back" to go back'
         input = gets.chomp
@@ -13,26 +15,24 @@ def user_stock_research_menu(rerun = 0)
         end
     elsif rerun == 1
         puts 'Sorry no match for that search. Try a different search term.'
-        user_stock
     end
-
-        puts 'Please enter stock symbol or stock name to lookup or type "back" to go back'
-        input = gets.chomp
-        if valid_symbol?(input)
-            user_stock_profile_menu(input)
-        elsif input == "back"
-            show_menu
-        else
-            print_recommendations(input)
-            user_stock_research_menu(2)
-        end
+    puts 'Please enter stock symbol or stock name to lookup or type "back" to go back'
+    input = gets.chomp
+    if valid_symbol?(input)
+        user_stock_profile_menu(input)
+    elsif input == "back"
+        show_menu
+    else
+        print_recommendations(input)
+        user_stock_research_menu(2)
+    end
 end
 
 def print_recommendations(input)
     recommendations = StockByName(input)
         if recommendations.length == 0
             system('clear')
-            user_stock_profile_menu(1)
+            user_stock_research_menu(1)
         else
             system('clear')
             puts 'Did you mean any of these stocks?'
